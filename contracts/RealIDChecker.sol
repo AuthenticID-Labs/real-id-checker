6c14bb1273f4531e70354ab20b3fd00fe4654cc0
Shawn Mitchell <shawn.mitchell>
2022-03-01 20:16:31 -0500

    add Merkle proofing

--- contracts/RealIDChecker.sol
+++ contracts/RealIDChecker.sol
@@ -25,4 +25,28 @@ contract RealIDChecker {
         require(msg.sender == owner, 'owner only');
         registrar = MyRegistrar(_registrarAddress);
     }
+
+    function verify(
+        address addy,
+        bytes32[] memory proof,
+        bytes32 leaf,
+        uint index
+    ) public view returns (bool) {
+        bytes32 root = registrar.getHash(addy);
+        bytes32 hash = leaf;
+
+        for (uint i = 0; i < proof.length; i++) {
+            bytes32 proofElement = proof[i];
+
+            if (index % 2 == 0) {
+                hash = sha256(abi.encodePacked(hash, proofElement));
+            } else {
+                hash = sha256(abi.encodePacked(proofElement, hash));
+            }
+
+            index = index / 2;
+        }
+
+        return hash == root;
+    }
 }
